d2d8b14b02d5669de531194e995b662960816526
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-12-15 16:49:46 -0500

    decoupled certs from education

--- src/cms-integration/getCMSIntegration.ts
+++ src/cms-integration/getCMSIntegration.ts
@@ -4,12 +4,14 @@ import { getLinks } from './markdown/links';
 import { getPersonalInformation } from './markdown/personal';
 import { getProfessionalExperiences } from './markdown/professional';
 import { getSkillCategories } from './markdown/skills';
+import { getCerts } from './markdown/certs';
 
 type CMS = 'markdown';
 
 const getCMSIntegration = async (cms: CMS) => {
   if (cms === 'markdown') {
     return {
+      certs: await getCerts(),
       education: await getEducationalExperiences(),
       hobbies: await getHobbies(),
       links: await getLinks(),
